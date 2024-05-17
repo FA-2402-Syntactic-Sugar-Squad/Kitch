b516a2e3f9adba977a1f7afc4c5fd3d635e853c4
Kelsey Cordero <kcordero99@gmail.com>
2024-05-17 12:45:12 -0400

    Displayed recipes on home page

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,12 +1,55 @@
-
-import AddReview from './components/AddReview.jsx';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Recipes = () => {
+  const [recipes, setRecipes] = useState([]);
+  const navigate = useNavigate();
+
+  const fetchAllRecipes = async () => {
+    try{
+      const result = await fetch(`/api/recipes/`);
+      // , {method: "GET",
+      // headers: {
+      //  "Content-Type": "application/json"},
+      // body: JSON.stringify({
+      //   id,
+      //   title,
+      //   instructions,
+      //   servings,
+      //   diets,
+      //   recipes_ingredients,
+      //   ratingsAndReviews
+      // }})
+      const recipeResult = await result.json();
+      setRecipes(recipeResult);
+      //console.log(recipes);
+    }catch(error){
+      console.error("Error when trying to fetch all recipes", error);
+    }
+  }
+  fetchAllRecipes();
+  
+  const handleClick = (recipeId) => {
+    navigate(`/recipes/${recipeId}`);
+  }
+  if (!recipes) {
+    return <h2>Loading..</h2>
+  }
+
   return (
     <>
       <h3>Recipes</h3>
       {/* DISPLAY ALL RECIPES ON Home Page */}
-      <AddReview/>
+      {recipes.map((curRecipe) => {
+        return (
+          <div key={curRecipe.id}> 
+            <p>{curRecipe.title}</p>
+            <p>{curRecipe.diets}</p>
+            <button onClick={() => handleClick(curRecipe.id)}>See Details</button>
+          </div>
+        )
+      })}
+      
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
+import Recipes from "../components/Recipes";
 
-const Home = () => {
+const Home = ({ token }) => {
   return (
     <>
       <h3>Home Page</h3>
       {/* Ternary: If non-logged in user clicks a recipe, prompt to login*/}
       {/* DISPLAY ALL RECIPES FROM RECIPES.JSX */}
       {/* DISPLAY ALL Ingredients FROM ingredients.JSX */}
+      <Recipes />
     </>
   )
 }
