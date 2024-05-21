a86317d1d787f15a093978aee8038df299fe806d
Kelsey Cordero <kcordero99@gmail.com>
2024-05-21 14:28:00 -0400

    to resolve merge conflict

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -26,6 +26,27 @@ const MyProfile = ({ token }) => {
     fetchProfile();
   }, []);
 
+  const handlePreferenceChange = (event) => {
+    const { name, checked } = event.target;
+    setPreferences(prev => ({ ...prev, [name]: checked }));
+  }
+
+  const savePreferences = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(`/api/users/preferences`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+      });
+      const updatedPreferences = await response.json();
+      setPreferences(updatedPreferences);
+    } catch (error) {
+      console.log("Error caught when fetching and updating preferences", error);
+    }
+  }
 
   return (
     <>
@@ -42,17 +63,24 @@ const MyProfile = ({ token }) => {
           </div>
           <div>
             <h3>Dietary Selection</h3>
-            <p>Gluten Free {preferences.glutenFree}</p>
-            <p>Ketogenic {preferences.ketogenic}</p>
-            <p>Lacto-Vegetarian {preferences.lactoVegetarian}</p>
-            <p>Low FODMAP {preferences.lowFODMAP}</p>
-            <p>Ovo-Vegetarian {preferences.ovoVegetarian}</p>
-            <p>Paleo {preferences.paleo}</p>
-            <p>Pescatarian {preferences.pescetarian}</p>
-            <p>Primal {preferences.primal}</p>
-            <p>Vegan {preferences.vegan}</p>
-            <p>Whole 30 {preferences.whole30}</p>     
+            <form>
+              {Object.keys(preferences).map(pref => (
+                <div key={pref}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name={pref}
+                      checked={preferences[pref]}
+                      onChange={handlePreferenceChange}
+                    />
+                    {pref.replace(/([A-Z])/g, '$1').trim()}
+                  </label>
+                </div>
+              ))}
+              <button type="button" onClick={savePreferences}>Save Preferences</button>
+            </form>
           </div>
+
         </>
       ) :
         <p>Loading...</p>}
