984fb767a53f38c2ee6473d4aaf44f6df41650a5
Kelsey Cordero <kcordero99@gmail.com>
2024-05-21 12:53:22 -0400

    updated index on my end to solve merge conflict

--- api/index.cjs
+++ api/index.cjs
@@ -5,9 +5,6 @@ apiRouter.get("/", (req, res) => {
   res.send("This is the root for /api")
 });
 
-//write route for ingredients
-const ingredientsRouter = require("./ingredients.cjs");
-apiRouter.use("/ingredients", ingredientsRouter);
 
 //write route for recipes
 const recipesRouter = require("./recipes.cjs");
@@ -16,6 +13,14 @@ apiRouter.use("/recipes", recipesRouter);
 //write route for users
 const usersRouter = require("./users.cjs");
 apiRouter.use("/users", usersRouter);
+
+//write route for ingredients
+const ingredientsRouter = require("./ingredients.cjs");
+apiRouter.use("/ingredients", ingredientsRouter);
+
+//write route for recipies by ingredient id
+const recipeByIngredientRouter = require("./recipesByIngredients.cjs");
+apiRouter.use("/recipes/byIngredient", recipeByIngredientRouter);
 
 //export
 module.exports = apiRouter;
