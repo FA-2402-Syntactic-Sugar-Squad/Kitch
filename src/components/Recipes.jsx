dbb00ab369e74a909f0f66ee15c1a0a0636896ba
Kelsey Cordero <kcordero99@gmail.com>
2024-05-22 12:12:51 -0400

    #102 body logger kept logging fetchallrecipes since it was not in a useEffect. Updated that. Body logger stopped logging.

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
   const navigate = useNavigate();
 
-  const fetchAllRecipes = async () => {
-    try{
-      const result = await fetch(`/api/recipes/`);
-      // , {method: "GET",
-      // headers: {
-      //  "Content-Type": "application/json"},
-      // body: JSON.stringify({
-      //   id,
-      //   title,
-      //   instructions,
-      //   servings,
-      //   diets,
-      //   recipes_ingredients,
-      //   ratingsAndReviews
-      // }})
-      const recipeResult = await result.json();
-      setRecipes(recipeResult);
-      //console.log(recipes);
-    }catch(error){
-      console.error("Error when trying to fetch all recipes", error);
+  useEffect(() => {
+    const fetchAllRecipes = async () => {
+      try {
+        const result = await fetch(`/api/recipes/`);
+        const recipeResult = await result.json();
+        setRecipes(recipeResult);
+        //console.log(recipes);
+      } catch (error) {
+        console.error("Error when trying to fetch all recipes", error);
+      }
     }
-  }
-  fetchAllRecipes();
-  
+    fetchAllRecipes();
+  }, []);
+
   const handleClick = (recipeId) => {
     navigate(`/recipes/${recipeId}`);
   }
@@ -42,14 +32,14 @@ const Recipes = () => {
       {/* DISPLAY ALL RECIPES ON Home Page */}
       {recipes.map((curRecipe) => {
         return (
-          <div key={curRecipe.id}> 
-            <p>{curRecipe.title}</p>
+          <div key={curRecipe.id}>
+            <h6>{curRecipe.title}</h6>
+            <img src={curRecipe.imageurl} />
             <p>{curRecipe.diets}</p>
             <button onClick={() => handleClick(curRecipe.id)}>See Details</button>
           </div>
         )
       })}
-      
     </>
   )
 }
