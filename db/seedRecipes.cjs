2ea15ee10102da07190750d9f61ab648b548f1be
wurus <160284767+wurus@users.noreply.github.com>
2024-05-22 15:54:24 -0400

    done

--- db/seedRecipes.cjs
+++ db/seedRecipes.cjs
@@ -1,15 +1,35 @@
-const axios = require('axios');
-const { PrismaClient } = require('@prisma/client');
+const axios = require("axios");
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 async function seedDatabase(numRecipes) {
   try {
-    const response = await axios.get(`https://api.spoonacular.com/recipes/random?number=${numRecipes}&apiKey=c89dd5c7bea0447594cf74b149427e34`);
+    const response = await axios.get(
+      `https://api.spoonacular.com/recipes/random?number=${numRecipes}&apiKey=c89dd5c7bea0447594cf74b149427e34`
+    );
     const recipes = response.data.recipes;
 
     for (const recipe of recipes) {
-      const { id: recipeId, title, analyzedInstructions, servings, extendedIngredients } = recipe;
+      const {
+        id: recipeId,
+        title,
+        analyzedInstructions,
+        servings,
+        extendedIngredients,
+        image: imageurl,
+        glutenFree,
+        ketogenic,
+        lactoVegetarian,
+        ovoVegetarian,
+        vegan,
+        pescetarian,
+        paleo,
+        primal,
+        lowFODMAP,
+        whole30,
+        "lacto ovo vegetarian": lactoOvoVegetarian,
+      } = recipe;
 
       const newRecipe = await prisma.recipes.create({
         data: {
@@ -17,12 +37,23 @@ async function seedDatabase(numRecipes) {
           title,
           instructions: generateInstructions(analyzedInstructions),
           servings,
+          imageurl: imageurl,
+          glutenFree: glutenFree || false,
+          ketogenic: ketogenic || false,
+          lactoVegetarian: lactoOvoVegetarian ? true : lactoVegetarian || false,
+          ovoVegetarian: lactoOvoVegetarian ? true : ovoVegetarian || false,
+          vegan: vegan || false,
+          pescetarian: pescetarian || false,
+          paleo: paleo || false,
+          primal: primal || false,
+          lowFODMAP: lowFODMAP || false,
+          whole30: whole30 || false,
         },
       });
 
       for (const ingredient of extendedIngredients) {
         const { id: ingredientId, name, aisle } = ingredient;
-        
+
         let existingIngredient = await prisma.ingredients.findUnique({
           where: { id: ingredientId },
         });
@@ -48,19 +79,19 @@ async function seedDatabase(numRecipes) {
       console.log(`Recipe "${title}" seeded successfully!`);
     }
 
-    console.log('Database seeding completed!');
+    console.log("Database seeding completed!");
   } catch (error) {
-    console.error('Error seeding database:', error);
+    console.error("Error seeding database:", error);
   } finally {
     await prisma.$disconnect();
   }
 }
 
 function generateInstructions(analyzedInstructions) {
-  let instructions = '';
+  let instructions = "";
 
-  analyzedInstructions.forEach(instruction => {
-    instruction.steps.forEach(step => {
+  analyzedInstructions.forEach((instruction) => {
+    instruction.steps.forEach((step) => {
       instructions += `${step.step}\n`;
     });
   });
@@ -68,5 +99,4 @@ function generateInstructions(analyzedInstructions) {
   return instructions;
 }
 
-seedDatabase(5);
-
+seedDatabase(20);
