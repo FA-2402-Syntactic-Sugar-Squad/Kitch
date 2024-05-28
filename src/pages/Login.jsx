d47446851666199fe72e71ac9984b95b8d2bce28
Kelsey Cordero <kcordero99@gmail.com>
2024-05-28 15:48:08 -0400

    #30 & #115 designed register and login layout on the front end.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
+import "../styling/Home_Register.css";
+
 const Login = ({ setToken }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(""); 
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const submitHandler = async (e) => {
@@ -17,16 +19,17 @@ const Login = ({ setToken }) => {
           "Accept": "application/json",
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ 
-          email, 
-          password })
+        body: JSON.stringify({
+          email,
+          password
+        })
       });
       const json = await result.json();
 
       if (result.status === 401) {
-        setError("Invalid email or password."); 
+        setError("Invalid email or password.");
       } else if (result.status === 500) {
-        setError("Email does not exist"); 
+        setError("Email does not exist");
       } else {
         if (json.token) {
           localStorage.setItem("token", json.token);
@@ -36,43 +39,47 @@ const Login = ({ setToken }) => {
       }
     } catch (error) {
       console.error("Error occurred:", error);
-      setError("An error occurred. Please try again later."); 
+      setError("An error occurred. Please try again later.");
     }
   };
 
   return (
     <>
-      <h1>Login</h1>
-      <form onSubmit={submitHandler}>
-        <label>
-          Email:
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </label>
-        <input type="submit" value="Log In" />
-      </form>
-      {error && <p style={{ color: "red" }}>{error}</p>} 
-      <label>
-        Don't have an account?
-        <button className="registerButton">
-        <Link to="/register">Sign up</Link>
-        </button>
-      </label>
+      <div className="Auth-form-container">
+        <form onSubmit={submitHandler} className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="text-center"> Don't have an account?{" "}<Link to="/register" className="link-primary">Sign up</Link></div>
+            <div className="form-group mt-3">
+              <label>Email</label>
+              <input
+                type="email"
+                placeholder="Enter email"
+                className="form-control mt-1"
+                value={email} onChange={(e) => { setEmail(e.target.value); }}
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                placeholder="Enter password"
+                className="form-control mt-1"
+                value={password} onChange={(e) => { setPassword(e.target.value); }}
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="text-center mt-2">
+              Forgot <a href="#">password?</a>
+            </p>
+          </div>
+        </form>
+      </div>
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </>
   );
 };
